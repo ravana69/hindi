4fee508a9d6ccbccb0cbe18606821b546631c527
Ravana <47528708+ravana69@users.noreply.github.com>
2021-10-12 13:04:58 +0530

    Update playback.js

--- playback.js
+++ playback.js
@@ -1,43 +1,47 @@
 const sources = [
-  "http://rd.shalombeatsradio.com:8006/stream", // Shalom Beats
-  "http://88.198.138.88:8000/jesusradio/;.m3u", // Jesus Radio Malayalam
-  "http://199.195.194.140:8012/stream", // Sehion Radio
-  "http://5.79.88.132:7084/;stream.mp3", // Psalms Radio
-  "https://dreamsiteradiocp2.com/proxy/rmindia1?mp=/stream", // Radio Maria
-  "https://ephphatha.radioca.st/stream?type=http&nocache=15206", // Ephphatha
-  "https://s2.radio.co/sf61790a26/listen", // Malayalam Christian Network
-  "https://stream.radio.co/s1383afdc9/listen?ver=52115", // Shalom
-  "http://live.jesuscomingfm.com:8132/;", // Jesus Coming FM
-  "http://5.79.88.132:7233/;type=mp3", // Gloria Radio Malayalam
-  "https://stream.radio.co/s9015a8e16/listen", // Radio Wind
-  "https://listen.radioking.com/radio/378295/stream/428998", // Tharangam Radio
-  "https://bozztv.com/36bay2/divine-event/tracks-v1a1/mono.m3u8", // DVN Radio
-  "http://66.55.145.43:7704/stream/1/", // Malayalam English Christian Radio
-  "https://gains.reviveradio.net/proxy/rafaradio?mp=/stream", // Rafa Radio
-  "http://radio.zionmediait.com:5327/;", // Kripa FM Bhilai
-  "https://gains.reviveradio.net/proxy/menorah?mp=/stream", // Menorah FM
-  "http://37.187.93.104:8586/stream" // Blessings Radio
+  "http://stream.zeno.fm/ukc8b9e5a48uv", // Ravana Hindi
+  "https://wmirchi-lh.akamaihd.net/i/WMIRCHI_1@75780/index_1_a-p.m3u8?sd=10&rebase=on&hdntl=exp=1634049515~acl=%2F*~data=hdntl~hmac=f3c3c49a4c4f7d4efbb1f140020c90ecc289d1c88ca2e43e9b45de586a294025", // Pehla Nasha
+  "https://meethimirchihdl-lh.akamaihd.net/i/MeethiMirchiHDLive_1_1@320572/index_1_a-p.m3u8?sd=10&rebase=on&hdntl=exp=1634050163~acl=%2F*~data=hdntl~hmac=05b3b5f288fd4d4fa567c425b8f1cad4a3edbef23d84b1846258c47c36387fac", // Meethi Mirchi
+  "https://tootadil-lh.akamaihd.net/i/tootadil_1@42539/index_1_a-p.m3u8?sd=10&rebase=on&hdntl=exp=1634050206~acl=%2F*~data=hdntl~hmac=5ad180be6f3c925309a209553e414109370072f19a50176877d283c3299df2f4", // Mirchi Toota Dil
+  "https://pehlanashahdlive-lh.akamaihd.net/i/PehlaNashaHDLive_1@335229/index_1_a-p.m3u8?sd=10&rebase=on", // Mirchi 90s Radio
+  "https://meethimirchihdl-lh.akamaihd.net/i/MirchiEdgeHDLive_1_1@336272/index_1_a-p.m3u8?sd=10&rebase=on&hdntl=exp=1634050314~acl=%2F*~data=hdntl~hmac=54b2d7f4712e5835b3a500f2136455680ce5f365a6a686d1c26651f417c36cc3", // Mirchi Indies
+  "https://filmymirchihdliv-lh.akamaihd.net/i/FilmyMirchiHDLive_1_1@336266/master.m3u8", // Filmy Mirchi
+  "https://puranijeanshdliv-lh.akamaihd.net/i/PuraniJeansHDLive_1_1@334555/index_1_a-p.m3u8?sd=10&rebase=on", // Purani Jeans
+  "https://clubmirchihdlive-lh.akamaihd.net/i/ClubMirchiHDLive_1_1@336269/master.m3u8", // Club Mirchi
+  "http://103.16.47.70:7222/;stream.mp3", // Hungama 90s
+  "https://air.pc.cdn.bitgravity.com/air/live/pbaudio001/playlist.m3u8", // Vividh Bharati
+  "https://prclive4.listenon.in/Ghazal", // Radio City Ghazal
+  "https://streams.radio.co/s69032f19e/listen", // Radio Selfie
+  "https://21293.live.streamtheworld.com/BIGRADIO_S01.mp3", // Bro Big Radio
+  "https://prclive4.listenon.in/Freedom", // Radio City Freedom
+  "https://prclive4.listenon.in/Electronica", // Radio City Electronica
+  "https://prclive4.listenon.in/Metal", // Radio City Metal
+  "hhttps://prclive4.listenon.in/HipHop, // Radio City Hip Hop
+  "https://prclive4.listenon.in/Dance", // Radio City Dance
+  "https://air.pc.cdn.bitgravity.com/air/live/pbaudio139/chunklist.m3u8" // AIR Ragam
 ];
 
 const labels = [
-  [ "Shalom Beats", "http://rd.shalombeatsradio.com:8006/stream"],
-  [ "Jesus Radio Malayalam", "http://88.198.138.88:8000/jesusradio/;.m3u"],
-  [ "Sehion Radio", "http://199.195.194.140:8012/stream"],
-  [ "Psalms Radio", "http://5.79.88.132:7084/;stream.mp3"],
-  [ "Radio Maria", "https://dreamsiteradiocp2.com/proxy/rmindia1?mp=/stream"],
-  [ "Ephphatha", "https://ephphatha.radioca.st/stream?type=http&nocache=15206"],
-  [ "Malayalam Christian Network", "https://s2.radio.co/sf61790a26/listen"],
-  [ "Shalom", "https://stream.radio.co/s1383afdc9/listen?ver=52115"],
-  [ "Jesus Coming FM", "http://live.jesuscomingfm.com:8132/;"],
-  [ "Gloria Radio Malayalam", "http://5.79.88.132:7233/;type=mp3"],
-  [ "Radio Wind", "https://stream.radio.co/s9015a8e16/listen"],
-  [ "Tharangam Radio", "https://listen.radioking.com/radio/378295/stream/428998"],
-  [ "DVN Radio", "https://bozztv.com/36bay2/divine-event/tracks-v1a1/mono.m3u8"],
-  [ "Malayalam English Christian Radio", "http://66.55.145.43:7704/stream/1/"],
-  [ "Rafa Radio", "https://gains.reviveradio.net/proxy/rafaradio?mp=/stream"],
-  [ "Kripa FM Bhilai", "http://radio.zionmediait.com:5327/;"],
-  [ "Menorah FM", "https://gains.reviveradio.net/proxy/menorah?mp=/stream"],
-  [ "Blessings Radio", "http://37.187.93.104:8586/stream"]
+  [ "Ravana Hindi", "http://stream.zeno.fm/ukc8b9e5a48uv"],
+  [ "Pehla Nasha", "https://wmirchi-lh.akamaihd.net/i/WMIRCHI_1@75780/index_1_a-p.m3u8?sd=10&rebase=on&hdntl=exp=1634049515~acl=%2F*~data=hdntl~hmac=f3c3c49a4c4f7d4efbb1f140020c90ecc289d1c88ca2e43e9b45de586a294025"],
+  [ "Meethi Mirchi", "https://meethimirchihdl-lh.akamaihd.net/i/MeethiMirchiHDLive_1_1@320572/index_1_a-p.m3u8?sd=10&rebase=on&hdntl=exp=1634050163~acl=%2F*~data=hdntl~hmac=05b3b5f288fd4d4fa567c425b8f1cad4a3edbef23d84b1846258c47c36387fac"],
+  [ "Mirchi Toota Dil", "https://tootadil-lh.akamaihd.net/i/tootadil_1@42539/index_1_a-p.m3u8?sd=10&rebase=on&hdntl=exp=1634050206~acl=%2F*~data=hdntl~hmac=5ad180be6f3c925309a209553e414109370072f19a50176877d283c3299df2f4"],
+  [ "Mirchi 90s Radio", "https://pehlanashahdlive-lh.akamaihd.net/i/PehlaNashaHDLive_1@335229/index_1_a-p.m3u8?sd=10&rebase=on"],
+  [ "Mirchi Indies", "https://meethimirchihdl-lh.akamaihd.net/i/MirchiEdgeHDLive_1_1@336272/index_1_a-p.m3u8?sd=10&rebase=on&hdntl=exp=1634050314~acl=%2F*~data=hdntl~hmac=54b2d7f4712e5835b3a500f2136455680ce5f365a6a686d1c26651f417c36cc3"],
+  [ "Filmy Mirchi", "https://filmymirchihdliv-lh.akamaihd.net/i/FilmyMirchiHDLive_1_1@336266/master.m3u8"],
+  [ "Purani Jeans", "https://puranijeanshdliv-lh.akamaihd.net/i/PuraniJeansHDLive_1_1@334555/index_1_a-p.m3u8?sd=10&rebase=on"],
+  [ "Club Mirchi", "https://clubmirchihdlive-lh.akamaihd.net/i/ClubMirchiHDLive_1_1@336269/master.m3u8"],
+  [ "Hungama 90s", "http://103.16.47.70:7222/;stream.mp3"],
+  [ "Vividh Bharati", "https://air.pc.cdn.bitgravity.com/air/live/pbaudio001/playlist.m3u8"],
+  [ "Radio City Ghazal", "https://prclive4.listenon.in/Ghazal"],
+  [ "Radio Selfie", "https://streams.radio.co/s69032f19e/listen"],
+  [ "Bro Big Radio", "https://21293.live.streamtheworld.com/BIGRADIO_S01.mp3"],
+  [ "Radio City Freedom", "https://prclive4.listenon.in/Freedom"],
+  [ "Radio City Electronica", "https://prclive4.listenon.in/Electronica"],
+  [ "Radio City Metal", "https://prclive4.listenon.in/Metal"],
+  [ "Radio City Hip Hop", "https://prclive4.listenon.in/HipHop"],
+  [ "Radio City Dance", "https://prclive4.listenon.in/Dance"],
+  [ "AIR Ragam", "https://air.pc.cdn.bitgravity.com/air/live/pbaudio139/chunklist.m3u8"]
 ];
 
 let playingIndex = 0; // current radio
